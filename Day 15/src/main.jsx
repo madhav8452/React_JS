25e181647d7c7d6e480fd14e53badd8ef9bff565
madhav8452 <madhavrathod019@gmail.com>
2025-02-14 15:19:11 +0530

    Sorting

---
+++ Day 15/src/main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import Sorting from './sorting.jsx'
+import Sort_asd_dsd from './Ascendind_descending_sort.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <>
+        <App />
+        <Sorting/>
+        <Sort_asd_dsd/>
+    </>
+)
